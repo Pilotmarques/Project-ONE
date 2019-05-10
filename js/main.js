1a849ef95af5ba962facd79334f0fb8549600564
Jose Marques <pilotmarquess@gmail.com>
2019-05-10 10:22:00 -0500

    most updated commit

--- js/main.js
+++ js/main.js
@@ -1,27 +1,35 @@
 
-// 1- this game has to hav only two gamers and they will play in rounds with each other
-// 2- each time, the gamer can toss the craps as many times as the gamer  wants
-// 3- each result gets added to the gamers round score board
-// 4- if you toss a one, then all you scores goes away!!
-// 5- then it is the next dudes turn
-// 6- the gamer can choose to hold. this means thaat the gamers score will get added to the over score the curent gamer has
-// 7- then if you hold it will be the next gamers turn
-// 8- the first gamer to rack up one hundred points on the over all score wins the battle!
+var scores, roundScore, activeGambler, craps;
 
-
-
-var scores, roundScores, activePlayer;
-
-scores = [0,0];
+scores = [0, 0];
 roundScore = 0;
-activePlayer = 0;
-
-craps = 6
+activeGambler = 0;
 
 
 
+document.querySelector('.craps').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 
 
+document.querySelector('.btn-toss').addEventListener('click', function() {
+    
+    var craps = Math.floor(Math.random() * 6) + 1;
+    var crapsDom = document.querySelector('.craps');
+    crapsDom.style.display = 'block';
+    crapsDom.src = './imgs/dice-' + craps + '.png';
+    
+    if (craps !== 1) {
+        roundScore += craps;
+        document.querySelector(`#current-${activeGambler}`).textContent = roundScore;
+    } else{
+        activeGambler === 0 ? activeGambler = 1 : activeGambler = 0;
+    }
+
+});
 
 
